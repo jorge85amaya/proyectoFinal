0c6e15938f0cc7c650a7bd7f782da136f8e78ff9
Jorge Amaya <jorgebenildoamaya@gmail.com>
2022-12-15 17:07:36 -0300

    agrega botones de wsp insta y facebook, crea un archivo .js y modifica estilos

---
+++ javaScript/script2.js
@@ -0,0 +1,12 @@
+let wsp = document.getElementById('img-wsp');
+let inst = document.getElementById('img-inst');
+let face = document.getElementById('img-face');
+wsp.addEventListener('click', ()=>{
+    alert("vas a salir de esta página.");
+});
+inst.addEventListener('click', ()=>{
+    alert("vas a salir de esta página.");
+});
+face.addEventListener('click', ()=>{
+    alert("vas a salir de esta página.");
+});
